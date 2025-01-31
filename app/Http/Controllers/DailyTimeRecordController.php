97eeb55d69ff6ba8338ab7aa9a1ffcb04d76939f
JoshDaguio <bennethpineda@gmail.com>
2025-01-31 22:47:51 +0800

    Fix Remaining Hours Calculation

--- app/Http/Controllers/DailyTimeRecordController.php
+++ app/Http/Controllers/DailyTimeRecordController.php
@@ -105,7 +105,7 @@ class DailyTimeRecordController extends Controller
 
     private function calculateFinishDate($remainingHours, $startDate, $schedule, $isIrregular = false)
     {
-        $date = $startDate; // Start calculation from the provided start date
+        $date = Carbon::now();
         $hoursRemaining = $remainingHours;
     
         $dailyWorkHours = [];
@@ -134,8 +134,10 @@ class DailyTimeRecordController extends Controller
     
             $start = Carbon::createFromTimeString($schedule['start_time']);
             $end = Carbon::createFromTimeString($schedule['end_time']);
-            $dailyHours = abs($end->diffInHours($start));
+            $dailyOfficeHours = abs($end->diffInHours($start));
     
+            $dailyHours = $dailyOfficeHours - 1;            
+
             // Combine onsite and remote days into one list of working days
             $workingDays = array_merge($schedule['onsite_days'], $schedule['remote_days']);
             $dailyWorkHours = array_fill_keys($workingDays, $dailyHours);
@@ -145,10 +147,10 @@ class DailyTimeRecordController extends Controller
         while ($hoursRemaining > 0) {
             $dayOfWeek = $date->format('l'); // Get the current day of the week
     
-            if (isset($dailyWorkHours[$dayOfWeek])) {
-                // Deduct hours worked for the current day
-                $hoursToday = $dailyWorkHours[$dayOfWeek];
-                $hoursRemaining -= $hoursToday;
+            // Only work Monday to Friday
+            if (in_array($dayOfWeek, ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'])) {
+                // Deduct 8 hours for the current working day
+                $hoursRemaining -= $dailyHours;
             }
     
             // Move to the next day
